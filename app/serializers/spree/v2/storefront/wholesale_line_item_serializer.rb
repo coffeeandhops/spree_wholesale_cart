d802d9cd7624951496887a0c6ea56f071a61ea8a
Jason Kroll <jason@coffeeandhops.com>
2020-11-08 14:07:30 +1000

    Clear bad paste

--- app/serializers/spree/v2/storefront/wholesale_line_item_serializer.rb
+++ app/serializers/spree/v2/storefront/wholesale_line_item_serializer.rb
@@ -1,15 +1,3 @@
-  module Spree
-    module WholesaleLineItemSerializerDecorator
-      def self.prepended(base)
-        base.attribute :total_wholesale_price, &:total_wholesale_price
-        base.attribute :display_wholesale_price, &:display_wholesale_price
-        base.attribute :is_wholesaleable, &:is_wholesaleable?
-        base.attribute :display_total_wholesale_price, &:display_total_wholesale_price
-      end
-    end
-  end
-
-Spree::V2::Storefront::LineItemSerializer.prepend SpreeWholesaleCart::Spree::LineItemSerializerDecorator
 module Spree
   module V2
     module Storefront
